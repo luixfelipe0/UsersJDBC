e87b0ac784062316af78f6d3d26d16e2e1af8a7c
luixfelipe0 <luiz_santos1811@outlook.com>
2025-09-19 11:09:41 -0300

    insert method implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,10 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.UserDao;
+import model.entities.User;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +16,8 @@ public class Main {
 
         System.out.println(userDao.findById(5));
         System.out.println(userDao.findByUsername("João Pereira"));
+
+        userDao.insert(new User(null, "luix", "luix.felipe@email.com", "adminadmin", new Date()));
+
     }
 }
--- src/model/dao/impl/UserDaoImplementation.java
+++ src/model/dao/impl/UserDaoImplementation.java
@@ -5,10 +5,9 @@ import db.DbException;
 import model.dao.UserDao;
 import model.entities.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,6 +22,45 @@ public class UserDaoImplementation implements UserDao {
 
     @Override
     public void insert(User u) {
+
+        PreparedStatement st = null;
+
+        try {
+
+            st = conn.prepareStatement(
+                    "INSERT INTO users "
+                            + "(username, email, password, birthDate) "
+                            + "VALUES "
+                            + "(?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, u.getUsername());
+            st.setString(2, u.getEmail());
+            st.setString(3, u.getPassword());
+            st.setDate(4, new java.sql.Date(u.getBirthDate().getTime()));
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    u.setId(id);
+                }
+                DBConfig.closeResultSet(rs);
+            }
+            else {
+                throw new DbException("Something went wrong. No rows affected.");
+            }
+
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DBConfig.closeStatement(st);
+        }
 
     }
 
